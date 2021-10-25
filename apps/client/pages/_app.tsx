e526ee4daaad92f0340ad75c42009bc16bac450c
Sebastian <sebastian.neumair@gmail.com>
2021-10-25 22:15:28 +0200

    move to scss & test styling changes

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import './styles.scss';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,7 +12,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <header className="flex">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to client!</h1>
+          <h1>Welcome to Archies Kingdom!</h1>
         </header>
         <main>
           <Component {...pageProps} />
