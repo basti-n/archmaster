711e9c45f680a2cd15de6d4d3e8f33b9906ddb35
Sebastian <sebastian.neumair@gmail.com>
2021-10-30 22:46:35 +0200

    add proper typings for index props (IArchie)

--- apps/client/specs/index.spec.tsx
+++ apps/client/specs/index.spec.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
 import Index from '../pages/index';
+import { IArchie } from '@archmaster/shared/types';
+
+const archie: IArchie = {
+  weight: 14,
+  weightUnit: 'kg',
+  dateOfBirth: '05.02.1992',
+  food: [],
+};
 
 describe('Index', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Index />);
+    const { baseElement } = render(<Index {...archie} />);
     expect(baseElement).toBeTruthy();
   });
 });
