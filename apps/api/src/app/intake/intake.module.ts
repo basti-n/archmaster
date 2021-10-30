7aed2313afd232a6d336a84dd84b2ba82a12b66e
Sebastian <sebastian.neumair@gmail.com>
2021-10-30 22:49:00 +0200

    add db connection enum

--- apps/api/src/app/intake/intake.module.ts
+++ apps/api/src/app/intake/intake.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DbCollection } from '../db/constants/collection.constants';
 import { Intake, IntakeSchema } from '../db/schema/intake.schema';
 import { IntakeService } from './intake.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Intake.name, schema: IntakeSchema, collection: 'intake' },
+      {
+        name: Intake.name,
+        schema: IntakeSchema,
+        collection: DbCollection.INTAKE,
+      },
     ]),
   ],
   controllers: [],
